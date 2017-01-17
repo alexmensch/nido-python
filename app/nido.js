1642eae05aff6347536cdbc65b54bdcfdf87376c
Alex Marshall <awm@alumni.stanford.edu>
2017-01-17 00:53:19 +0000

    Totally arbitrary intermediate work in progress state for React code

--- app/nido.js
+++ app/nido.js
@@ -8,8 +8,16 @@ import "isomorphic-fetch";
  * ********
  */
 
-function fetchCheckStatus(response) {  
+function fetchCheckStatus2xx(response) {  
     if (response.status >= 200 && response.status < 300) {  
+        return Promise.resolve(response)
+    } else {  
+        return Promise.reject(new Error(response.statusText))
+    }  
+}
+
+function fetchCheckStatus2xx403(response) {  
+    if (response.status == 403 || response.status >= 200 && response.status < 300) {  
         return Promise.resolve(response)
     } else {  
         return Promise.reject(new Error(response.statusText))
@@ -29,22 +37,20 @@ function fetchGenericError(error) {
  * ********
  */
 
-function fetchData(updateFunc, route, key) {
-    fetch('/' + route, {
+function fetchData(route, key) {
+    return fetch('/' + route, {
         method: 'POST',
         credentials: 'include'
     })
-    .then(fetchCheckStatus)
+    .then(fetchCheckStatus2xx)
     .then(fetchResponseJSON)
     .then(function(json) {
         if (json) {
-            updateFunc(json, key);
+            return Promise.resolve({json: json, key: key});
         } else {
             return Promise.reject(new Error('No JSON in reponse body.'))
         }
     }).catch(fetchGenericError);
-    
-    return
 }
 
 /* ********
@@ -52,63 +58,221 @@ function fetchData(updateFunc, route, key) {
  * ********
  */
 
-var Dashboard = React.createClass({
-    getInitialState: function() {
-        return {
-            config: undefined,
-            state: undefined,
-            weather: undefined,
-            user: this.props.userInfo
-        };
-    },
+var FormButton = React.createClass({
+    // Received props:
+    // buttonText, clickAction (func), buttonClass, divClass
+    render: function() {
+        return (
+            <div className="form-group">
+                <div className={this.props.divClass}>
+                    <button type="button" onClick={this.props.clickAction}
+                            className={'btn ' + this.props.buttonClass}>{this.props.buttonText}</button>
+                </div>
+            </div>
+            );
+    }
+});
 
-    updateDashboardState: function(data, key) {
-        if ( key in data ) {
-            this.setState({
-                [key]: data[key]
-            });
-        } else if ( 'error' in data ) {
-            this.setState({
-                [key]: data
-            });
-        } else {
-            throw('Unexpected error retrieving ' + key + ' data. Server returned: ' + data);
+var FormRow = React.createClass({
+    // Received props:
+    // labelClass, inputDivClass, inputType, inputPlaceholder (string or list), labelText
+    render: function() {
+        var input_id = this.props.inputId ? this.props.inputId : 'input-' + this.props.labelText;
+        switch(this.props.inputType) {
+            case 'text':
+            case 'password':
+                return (
+                    <div className="form-group">
+                        <label htmlFor={input_id} className={'control-label ' + this.props.labelClass}>{this.props.labelText}</label>
+                        <div className={this.props.inputDivClass}>
+                            <input type={this.props.inputType} className="form-control" id={input_id}
+                                   placeholder={this.props.inputPlaceholder} />
+                        </div>
+                    </div>
+                    );
+            case 'checkbox':
+            case 'radio':
+                return (
+                    <div className="form-group">
+                        <label htmlFor={input_id} className={'control-label ' + this.props.labelClass}>{this.props.labelText}</label>
+                        <div id={input_id} className={this.props.inputDivClass} >
+                            {this.props.inputPlaceholder.map((item) =>
+                                <div className={this.props.inputType} key={item}>
+                                    <label>
+                                        <input type={this.props.inputType} name={this.props.inputType + '-' + this.props.labelText}
+                                            id={input_id + '-' + item} value={item} />
+                                        {item}
+                                    </label>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    );
+            case 'select':
+                return (
+                    <div className="form-group">
+                        <label htmlFor={input_id} className={'control-label ' + this.props.labelClass}>{this.props.labelText}</label>
+                        <select className="form-control" name={this.props.inputType + '-' + this.props.labelText}
+                                id={input_id} className={this.props.inputDivClass} >
+                            {this.props.inputPlaceholder.map((item) =>
+                               <option value={item} key={item}>{item}</option> 
+                            )}
+                        </select>
+                    </div>
+                       );
+            case 'textarea':
+                return (
+                    <div className="form-group">
+                        <label htmlFor={input_id} className={'control-label ' + this.props.labelClass}>{this.props.labelText}</label>
+                        <div className={this.props.inputDivClass}>
+                            <textarea className="form-control" id={input_id} rows="3">{this.props.inputPlaceholder}</textarea>
+                        </div>
+                    </div>
+                       );
+            default:
+                throw('Unexpected inputType property passed to FormRow component: ' + this.props.inputType);
         }
-    },
+    }
+});
 
-    componentDidMount: function() {
-        // This component only loads if the user is logged in
-        // Make calls to get_config, get_state, get_weather and get_user (if necessary)
-        fetchData(this.updateDashboardState, 'get_config', 'config');
-        fetchData(this.updateDashboardState, 'get_state', 'state');
-        fetchData(this.updateDashboardState, 'get_weather', 'weather');
-        if ( this.state.user == undefined ) {
-            fetchData(this.updateDashboardState, 'get_user', 'user');
+var FormRowsFromObject = React.createClass({
+    // Received props:
+    // data (object), labelClass, inputDivClass
+    render: function() {
+        var formRows = [];
+        for (var row in this.props.data) {
+            if (this.props.data.hasOwnProperty(row)) {
+                formRows.push(row);
+            }
         }
+
+        return (
+            <div className="configFormRows">
+            {formRows.map((key) =>
+                <FormRow labelClass={this.props.labelClass} inputDivClass={this.props.inputDivClass}
+                         inputType={this.props.data[key].form_data[0]}
+                         inputPlaceholder={this.props.data[key].form_data[1]}
+                         labelText={this.props.data[key].label}
+                         key={key} />
+                         )}
+            </div>
+            );
+    }
+});
+
+var Config = React.createClass({
+    // Props: setView (function), config (JSON)
+    onConfigSubmit: function() {
+        return
     },
 
     render: function() {
+        return (
+                <div id="config">
+                    <h2>Nido Configuration</h2>
+                    <form className="form-horizontal">
+                        <FormRowsFromObject data={this.props.config.config}
+                                            labelClass="col-sm-2" inputDivClass="col-sm-3" />
+                        <FormButton buttonText="Save" clickAction={this.onConfigSubmit}
+                                    buttonClass="btn-default" divClass="col-sm-offset-2 col-sm-2" />
+                    </form>
+                </div>
+               );
+    }
+});
+
+var Toolbar = React.createClass({
+    // Props: setView (function)
+    configAction: function() {
+        return
+    },
+
+    logoutAction: function() {
+        return
+    },
+
+    render: function() {
+        return (
+                <div id="toolbar">
+                    <ToolbarButton icon={undefined} action={this.configAction} />
+                    <ToolbarButton icon={undefined} action={this.logoutAction} />
+                </div>
+                );
+    }
+});
+
+var ToolbarButton = React.createClass({
+    // Props: icon, action (function)
+    render: function() {
+        return <span>ToolbarButton</span>;
+    }
+});
+
+var Dashboard = React.createClass({
+    // Props: setView (function), config (JSON), state (JSON), weather (JSON)
+    render: function() {
         return(
                 <div>
-                <p><strong>Config:</strong></p>
-                <p>{JSON.stringify(this.state.config)}</p>
-                <p><strong>State:</strong></p>
-                <p>{JSON.stringify(this.state.state)}</p>
-                <p><strong>Weather:</strong></p>
-                <p>{JSON.stringify(this.state.weather)}</p>
-                <p><strong>User:</strong></p>
-                <p>{JSON.stringify(this.state.user)}</p>
+                    <Toolbar setView={this.props.setView} />
+                    <div id="dashboard">
+                        <NidoState state={this.props.state} config={this.props.config} />
+                        <Weather weather={this.props.weather} />
+                    </div>
                 </div>
               );
     }
 });
 
-var LoginForm = React.createClass({
+var NidoState = React.createClass({
+    // Props: state (JSON), config (JSON)
+    render: function() {
+        return(
+            <div>
+                <div id="nidoState">
+                    <div><strong>State:</strong>{JSON.stringify(this.props.state)}</div>
+                    <div><strong>Config:</strong>{JSON.stringify(this.props.config)}</div>
+                </div>
+                <div id="userControls">
+                    <div id="changeSetpoint"><ChangeSetpoint config={this.props.config} /></div>
+                    <div id="onOff"><OnOff config={this.props.config} /></div>
+                </div>
+            </div>
+            );
+    }
+});
+
+var ChangeSetpoint = React.createClass({
+    // Props: config (JSON)
+    render: function() {
+        return <span>ChangeSetpoint</span>;
+    }
+});
+
+var OnOff = React.createClass({
+    // Props: config (JSON)
+    render: function() {
+        return <span>OnOff</span>;
+    }
+});
+
+var Weather = React.createClass({
+    // Props: weather (JSON)
+    render: function() {
+        return(
+            <div id="weather">
+                <div><strong>Weather:</strong>{JSON.stringify(this.props.weather)}</div>
+            </div>
+            );
+    }
+});
+
+var Login = React.createClass({
+    // Login: setView (function)
     onLoginSubmit: function() {
         // Set up username/password form data
         var form = new FormData();
-        form.append("username", document.getElementById('inputUsername').value);
-        form.append("password", document.getElementById('inputPassword').value);
+        form.append("username", document.getElementById('input-Username').value);
+        form.append("password", document.getElementById('input-Password').value);
        
         // Preserve scope 
         var that = this;
@@ -118,12 +282,12 @@ var LoginForm = React.createClass({
             body: form,
             credentials: 'include'
         })
-        .then(fetchCheckStatus)
+        .then(fetchCheckStatus2xx)
         .then(fetchResponseJSON)
         .then(function(json) {
             // TODO: Incorporate message/error text into user feedback.
-            if (json['logged_in']) {
-                that.props.setLogin(true);
+            if ('logged_in' in json) {
+                that.props.setView('dashboard');
             }
         }).catch(fetchGenericError);
     },
@@ -133,27 +297,15 @@ var LoginForm = React.createClass({
             <div className="login">
                 <h2>Login</h2>
                 <form className="form-horizontal">
-                    <div className="form-group">
-                        <label htmlFor="inputUsername" className="col-sm-2 control-label">Username</label>
-                        <div className="col-sm-3">
-                            <input type="text" className="form-control" id="inputUsername" placeholder="Username" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="inputPassword" className="col-sm-2 control-label">Password</label>
-                        <div className="col-sm-3">
-                            <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-offset-2 col-sm-2">
-                            <button type="button" onClick={this.onLoginSubmit} className="btn btn-default">Sign in</button>
-                        </div>
-                    </div> 
+                    <FormRow labelText="Username" labelClass="col-sm-2" inputId="input-Username"
+                             inputDivClass="col-sm-3" inputType="text" inputPlaceholder="Username" />
+                    <FormRow labelText="Password" labelClass="col-sm-2" inputId="input-Password"
+                             inputDivClass="col-sm-3" inputType="password" inputPlaceholder="Password" />
+                    <FormButton buttonText="Sign in" clickAction={this.onLoginSubmit}
+                                buttonClass="btn-default" divClass="col-sm-offset-2 col-sm2" />
                 </form>
             </div>
-            );
-    }
+            )}
 });
 
 var Loading = React.createClass({
@@ -165,73 +317,84 @@ var Loading = React.createClass({
 var Nido = React.createClass({
     getInitialState: function() {
         return {
-            loadingState: true,
-            loginState: false,
-            userInfo: undefined
+            view: 'loading',
+            config: undefined,
+            state: undefined,
+            weather: undefined
         };
     },
 
-    setLoginState: function(state) {
+    setView: function(state) {
         this.setState({
-            loginState: state
+            view: state
         });
     },
 
-    setLoadingState: function(state) {
-        this.setState({
-            loadingState: state
-        });
-    },
-
-    setUserInfo: function(info) {
-        this.setState({
-            userInfo: info
-        });
+    updateState: function(responseObject) {
+        var newState = {};
+        newState[responseObject.key] = responseObject.json;
+        this.setState(newState);
     },
 
     componentDidMount: function() {
         // Preserve scope 
         var that = this;
-        // Make a request to /get_user and check response to determine what to do next
-        fetch('/get_user', {
+        fetch('/get_config', {
             method: 'POST',
             credentials: 'include'
         })
-        // Valid response status?
-        .then(function(response) {
-            if ( (response.status == 403) || (response.status >= 200 && response.status < 300) ) {
-                return Promise.resolve(response)
-            } else {  
-                return Promise.reject(new Error(response.statusText))
-            }  
-        })
-        // Is the user logged in?
+        .then(fetchCheckStatus2xx403)
         .then(function(response) {
             if ( response.status == 403 ) {
-                that.setLoginState(false);
-                that.setLoadingState(false);
+                that.setView('login');
+                return Promise.resolve();
             } else {
-                that.setLoginState(true);
-                response.json().then(function(json) {
-                    if ( 'user' in json ) {
-                        that.setUserInfo(json['user']);
-                    }
-                    that.setLoadingState(false);
-                });
+                return response.json();
+            }
+        })
+        .then(function(json) {
+            // Return here if we got a 403 response
+            if (json == undefined) return Promise.resolve();
+
+            // Fetch state and weather info
+            fetchData('get_state', 'state')
+                .then(that.updateState);
+            fetchData('get_weather', 'weather')
+                .then(that.updateState);
+            
+            // Set config state
+            that.setState({
+                config: json
+            });
+
+            // Set Dashboard or Config view depending on get_config response
+            if ( 'config_required' in json ) {
+                that.setView('config');
+            } else {
+                that.setView('dashboard');
             }
         })
         .catch(fetchGenericError);
     },
     
     render: function() {
-        if (this.state.loadingState) {
-            return <Loading />;
-        } else {
-            if (this.state.loginState) {
-                return <Dashboard setLogin={this.setLoginState} userInfo={this.state.userInfo} />;
-            } else {
-                return <LoginForm setLogin={this.setLoginState} />;
-            }
+        switch(this.state.view) {
+            case 'loading':
+                return <Loading />;
+                break;
+            case 'login':
+                return <Login setView={this.setView} />;
+                break;
+            case 'dashboard':
+                return <Dashboard setView={this.setView} config={this.state.config}
+                                                         state={this.state.state}
+                                                         weather={this.state.weather} />;
+                break;
+            case 'config':
+                return <Config setView={this.setView} config={this.state.config} />;
+                break;
+            default:
+                throw('Unexpected state was set in Nido component: ' + this.state.view);
         }
     }
 });
