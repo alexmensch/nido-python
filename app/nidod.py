c3398816257dfd3d67beb922226f080ee70577b8
Alex Marshall <awm@alumni.stanford.edu>
2018-04-21 20:01:26 -0700

    Removed excessive exception handling

--- app/nidod.py
+++ app/nidod.py
@@ -8,38 +8,25 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 class NidoDaemon(Daemon):
     def run(self):
-        # Get poll interval from config
-        try:
-            config = Config().get_config()
-        except Exception as e:
-            sys.stderr.write('{} [Error] Unable to read configuration file: {}\n'.format(datetime.utcnow(), e))
-            self.stop()
-        else:
-            try:
-                poll_interval = config['daemon']['poll_interval']
-            except Exception as e:
-                sys.stderr.write('{} [Error] Unable to read daemon poll_interval from config file: {}\n'.format(datetime.utcnow(), e))
-                self.stop()
-
         # Instantiate controller object
-        try:
-            self.controller = Controller()
-        except Exception as e:
-            sys.stderr.write('{} [Error] Unable to instantiate Controller: {}\n'.format(datetime.utcnow(), e))
-            self.stop()
+        self.controller = Controller()
+        # Set up scheduler
+        self.scheduler = BlockingScheduler({
+            'apscheduler.job_defaults.coalesce': 'true',
+            })
+
+        # Get poll interval from config
+        config = Config().get_config()
+        poll_interval = config['daemon']['poll_interval']
+        # Add scheduled job on configured polling interval
+        self.scheduler.add_job(self.controller.update, trigger='interval', seconds=poll_interval)
 
         # Set up signal handler to trigger updates
         signal.signal(signal.SIGUSR1, self.signal_handler)
         # Log start time
         sys.stdout.write('{} [Info] Nido daemon started\n'.format(datetime.utcnow()))
         sys.stdout.flush()
-
-        ###
-        # Set up scheduler
-        self.scheduler = BlockingScheduler({
-            'apscheduler.job_defaults.coalesce': 'true',
-            })
-        self.scheduler.add_job(self.controller.update, id='poll', trigger='interval', seconds=poll_interval)
+        # Start scheduler (blocking)
         self.scheduler.start()
 
     def signal_handler(self, signum, stack):
@@ -52,14 +39,8 @@ class NidoDaemon(Daemon):
 
     def quit(self):
         self.scheduler.shutdown(wait=False)
-        try:
-            Controller().shutdown()
-        except Exception as e:
-            sys.stderr.write('{} [Error] *CRITICAL*: unable to shutdown GPIO pins: {}\n'.format(datetime.utcnow(), e))
-        else:
-            # Log stop time
-            sys.stdout.write('{} [Info] Nido daemon shutdown\n'.format(datetime.utcnow()))
-            sys.stdout.flush()
+        Controller().shutdown()
+        sys.stdout.write('{} [Info] Nido daemon shutdown\n'.format(datetime.utcnow()))
         return
 
 ###
