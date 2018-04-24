9a3d832fbc731414a5279404257f44d8ca0861c7
Alex Marshall <alexm@Alexs-QZY-MBP.local>
2018-04-24 08:47:15 -0700

    Intermediate change to nidod for RPyC config

--- app/nidod.py
+++ app/nidod.py
@@ -9,6 +9,36 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import rpyc
 from rpyc.utils.server import ThreadedServer
 
+# Adapted from: https://github.com/agronholm/apscheduler/blob/master/examples/rpc/server.py
+#
+class SchedulerService(rpyc.Service):
+    def __init__(self, scheduler):
+        self.scheduler = scheduler
+
+    def exposed_add_job(self, func, *args, **kwargs):
+        return self.scheduler.add_job(func, *args, **kwargs)
+
+    def exposed_modify_job(self, job_id, jobstore=None, **changes):
+        return self.scheduler.modify_job(job_id, jobstore, **changes)
+
+    def exposed_reschedule_job(self, job_id, jobstore=None, trigger=None, **trigger_args):
+        return self.scheduler.reschedule_job(job_id, jobstore, trigger, **trigger_args)
+
+    def exposed_pause_job(self, job_id, jobstore=None):
+        return self.scheduler.pause_job(job_id, jobstore)
+
+    def exposed_resume_job(self, job_id, jobstore=None):
+        return self.scheduler.resume_job(job_id, jobstore)
+
+    def exposed_remove_job(self, job_id, jobstore=None):
+        self.scheduler.remove_job(job_id, jobstore)
+
+    def exposed_get_job(self, job_id):
+        return self.scheduler.get_job(job_id)
+
+    def exposed_get_jobs(self, jobstore=None):
+        return self.scheduler.get_jobs(jobstore)
+
 class NidoDaemon(Daemon):
     def run(self):
         # Get config
@@ -46,36 +76,6 @@ class NidoDaemon(Daemon):
         Controller().shutdown()
         sys.stdout.write('{} [Info] Nido daemon shutdown\n'.format(datetime.utcnow()))
         return
-
-# Adapted from: https://github.com/agronholm/apscheduler/blob/master/examples/rpc/server.py
-#
-class SchedulerService(rpyc.Service):
-    def __init__(self, scheduler):
-        self.scheduler = scheduler
-
-    def exposed_add_job(self, func, *args, **kwargs):
-        return self.scheduler.add_job(func, *args, **kwargs)
-
-    def exposed_modify_job(self, job_id, jobstore=None, **changes):
-        return self.scheduler.modify_job(job_id, jobstore, **changes)
-
-    def exposed_reschedule_job(self, job_id, jobstore=None, trigger=None, **trigger_args):
-        return self.scheduler.reschedule_job(job_id, jobstore, trigger, **trigger_args)
-
-    def exposed_pause_job(self, job_id, jobstore=None):
-        return self.scheduler.pause_job(job_id, jobstore)
-
-    def exposed_resume_job(self, job_id, jobstore=None):
-        return self.scheduler.resume_job(job_id, jobstore)
-
-    def exposed_remove_job(self, job_id, jobstore=None):
-        self.scheduler.remove_job(job_id, jobstore)
-
-    def exposed_get_job(self, job_id):
-        return self.scheduler.get_job(job_id)
-
-    def exposed_get_jobs(self, jobstore=None):
-        return self.scheduler.get_jobs(jobstore)
 
 ###
 # Start of execution
