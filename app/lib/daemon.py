8d41f28473cf62250955eee283b531cda4a375bc
Alex Marshall <awm@alumni.stanford.edu>
2018-05-17 10:31:05 -0700

    New requirements file for python3; updated file I/O in daemon library to be python3 compatible

--- app/lib/daemon.py
+++ app/lib/daemon.py
@@ -93,9 +93,9 @@ class Daemon:
         # redirect standard file descriptors
         sys.stdout.flush()
         sys.stderr.flush()
-        si = file(self.stdin, 'r')
-        so = file(self.stdout, 'a+')
-        se = file(self.stderr, 'a+', 0)
+        si = open(self.stdin, 'r')
+        so = open(self.stdout, 'a+')
+        se = open(self.stderr, 'a+')
         os.dup2(si.fileno(), sys.stdin.fileno())
         os.dup2(so.fileno(), sys.stdout.fileno())
         os.dup2(se.fileno(), sys.stderr.fileno())
@@ -108,7 +108,8 @@ class Daemon:
 
         # write pidfile
         pid = str(os.getpid())
-        file(self.pidfile, 'w+').write("%s\n" % pid)
+        with open(self.pidfile, 'w+') as f:
+            f.write("%s\n" % pid)
 
     def onstop(self):
         self.quit()
@@ -120,9 +121,8 @@ class Daemon:
         """
         # Check for a pidfile to see if the daemon already runs
         try:
-            pf = file(self.pidfile, 'r')
-            pid = int(pf.read().strip())
-            pf.close()
+            with open(self.pidfile, 'r') as pf:
+                pid = int(pf.read().strip())
         except IOError:
             pid = None
 
@@ -141,9 +141,8 @@ class Daemon:
         """
         # Get the pid from the pidfile
         try:
-            pf = file(self.pidfile, 'r')
-            pid = int(pf.read().strip())
-            pf.close()
+            with open(self.pidfile, 'r') as pf:
+                pid = int(pf.read().strip())
         except IOError:
             pid = None
 
