e085a958dbc7abcc62eccaf4dd45362b11fbb787
Alex Marshall <awm@alumni.stanford.edu>
2018-05-22 23:00:25 -0700

    Added missing setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
 from setuptools import find_packages, setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='nido',
+    version='0.2.0',
+    author='Alex Marshall',
+    author_name='alex@moveolabs.com',
+    description='Nido, a Raspberry Pi-based home thermostat',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alexmensch/nido',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3'
+        ' or later (GPLv3+)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
