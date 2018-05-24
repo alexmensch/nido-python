412f3ead7307d85ee6d5129785fbda5f5d516d8f
Alex Marshall <awm@alumni.stanford.edu>
2018-05-23 23:36:21 -0700

    Updates to install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+echo "Setting up python3 venv..."
 python3 -m venv venv && source venv/bin/activate
-pip install --upgrade pip
-npm install
-npm run build-prod
+
+echo "Creating local adhoc SSL certificates..."
 mkdir instance
 openssl req -x509 -newkey rsa:4096 -nodes -out instance/nido_cert.pem -keyout instance/nido_key.pem -days 365
+
+pip install --upgrade pip
 pip install -e .
 pip install -r requirements.txt
+
+npm install
+npm run build-prod
+
+echo
 echo "********"
 echo "To complete installation you must create:"
 echo " instance/private-config.py"
