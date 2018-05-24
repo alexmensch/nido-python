b90aa58cafc42a737bbcb29de80b405ac46dc807
Alex Marshall <awm@alumni.stanford.edu>
2018-05-23 23:13:01 -0700

    Moved adhoc SSL cert creation to install.sh

--- create-adhoc-certs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-openssl req -x509 -newkey rsa:4096 -nodes -out instance/nido_cert.pem -keyout instance/nido_key.pem -days 365
--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ python3 -m venv venv && source venv/bin/activate
 pip install --upgrade pip
 npm install
 npm run build-prod
+mkdir instance
+openssl req -x509 -newkey rsa:4096 -nodes -out instance/nido_cert.pem -keyout instance/nido_key.pem -days 365
