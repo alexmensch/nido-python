9364170a36ba60ace6a15b4030d8541a4be3b998
Alex Marshall <awm@alumni.stanford.edu>
2018-05-28 16:27:44 -0700

    Final futurize

--- nidod/db.py
+++ nidod/db.py
@@ -16,6 +16,8 @@
 #   along with this program.
 #   If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
+
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine, Column, Integer, Boolean, Float
--- nidod/lib/thermostat.py
+++ nidod/lib/thermostat.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from builtins import object
 
 import logging
