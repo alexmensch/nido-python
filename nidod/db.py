470cacc82b39cb827a4e37c9c168e2288aa85f54
Alex Marshall <awm@alumni.stanford.edu>
2018-05-27 18:24:36 -0700

    Changes to init_db

--- nidod/db.py
+++ nidod/db.py
@@ -1,23 +1,24 @@
-# from contextlib import contextmanager
+#!/usr/bin/python
+
+#   Nido, a Raspberry Pi-based home thermostat.
 #
-# @contextmanager
-# def session_scope():
-#     """Provide a transactional scope around a series of operations."""
-#     session = Session()
-#     try:
-#         yield session
-#         session.commit()
-#     except:
-#         session.rollback()
-#         raise
-#     finally:
-#         session.close()
+#   Copyright (C) 2016 Alex Marshall
 #
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-# def run_my_program():
-#     with session_scope() as session:
-#         ThingOne().go(session)
-#         ThingTwo().go(session)
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.
+#   If not, see <http://www.gnu.org/licenses/>.
+
+from contextlib import contextmanager
 
 from sqlalchemy import create_engine, Column, Integer, Boolean, Float
 from sqlalchemy.ext.declarative import declarative_base
@@ -44,13 +45,32 @@ class Settings(Base):
         )
 
 
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
 def init_db(base, engine):
+    session = Session()
+    # Need to check if table exists with a query
+    # If it does, overwrite what's there, otherwise create it
+    #     with session_scope() as session:
+    #         ThingOne().go(session)
+    #         ThingTwo().go(session)
     base.metadata.create_all(engine)
     default_settings = Settings(set_temp=21.0,
                                 set_mode=Mode.Off.value,
                                 celsius=False)
     print('Initializing default settings: {}'.format(default_settings))
-    session = Session()
     session.add(default_settings)
     session.commit()
 
