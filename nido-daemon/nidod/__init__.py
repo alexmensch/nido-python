ad9b0e8a87cb912d3ed4b9bc52e47808e947c3d6
Alex Marshall <awm@alumni.stanford.edu>
2019-07-14 00:26:28 -0700

    Converted Nido daemon to regular process

--- nido-daemon/nidod/__init__.py
+++ nido-daemon/nidod/__init__.py
@@ -16,189 +16,106 @@
 #   along with this program.
 #   If not, see <http://www.gnu.org/licenses/>.
 
-#####
-
-#   From "A simple unix/linux daemon in Python" by Sander Marechal
-#   See http://stackoverflow.com/a/473702/1422096
-#
-#   Modified to add quit() that allows to run some code before closing
-#   the daemon. See: http://stackoverflow.com/a/40423758/1422096
-#
-#   Joseph Ernest, 2016/11/12
-#
-#   Modified to force instantiator of Daemon class to specify pidfile
-#   and working directory
-#
-#   Alex Marshall, 2016/12/03
-
-from __future__ import print_function
 from __future__ import unicode_literals
+from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 from future import standard_library
 
 standard_library.install_aliases()
 from builtins import *
-from builtins import str
-from builtins import object
 
 import sys
-import os
-import time
-import atexit
 import logging
-from signal import signal, SIGTERM
+import logging.handlers
+import os
+import atexit
+
+import paho.mqtt.client as mqtt
+from rpyc.utils.server import ThreadedServer
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from nidod import Daemon
+from nidod.config import SchedulerConfig, DaemonConfig, MQTTConfig
+from nidod.hardware import Controller
+from nidod.rpc.server import NidoDaemonService
 
 
-class Daemon(object):
-    """
-    A generic daemon class.
+def run():
+    self._l.debug("Starting Nido controller...")
+    self.controller = Controller()
 
-    Usage: subclass the Daemon class and override the run()
-    and quit() methods.
-    """
+    self.MQTTclient = mqtt.Client(MQTTConfig.CLIENT_NAME, clean_session=False)
+    self.MQTTclient.enable_logger()
+    self.MQTTclient.connect_async(
+        MQTTConfig.HOSTNAME,
+        port=int(MQTTConfig.PORT),
+        keepalive=MQTTConfig.KEEPALIVE,
+    )
 
-    def __init__(
-        self,
-        pidfile,
-        workdir,
-        stdin="/dev/null",
-        stdout="/dev/null",
-        stderr="/dev/null",
-    ):
-        self.stdin = stdin
-        self.stdout = stdout
-        self.stderr = stderr
-        self.pidfile = pidfile
-        self.workdir = workdir
-        self._l = logging.getLogger(__name__)
+    self.scheduler = BackgroundScheduler()
+    jobstores = {
+        "default": {"type": "memory"},
+        "schedule": SQLAlchemyJobStore(
+            url="sqlite:///{}".format(DaemonConfig.DB_PATH)
+        ),
+    }
+    job_defaults = {"coalesce": True, "misfire_grace_time": 10}
+    self.scheduler.configure(jobstores=jobstores, job_defaults=job_defaults)
+    self.scheduler.add_job(NidoDaemonService.wakeup, name="Poll")
+    self.scheduler.add_job(
+        NidoDaemonService.wakeup,
+        trigger="interval",
+        seconds=SchedulerConfig.POLL_INTERVAL,
+        name="Poll",
+    )
+    self.scheduler.add_job(
+        NidoDaemonService.log_data,
+        args=[self.MQTTclient],
+        trigger="interval",
+        seconds=MQTTConfig.POLL_INTERVAL,
+        name="DataLogger",
+    )
 
-    def daemonize(self):
-        """
-        Do the UNIX double-fork magic.
+    self.RPCserver = ThreadedServer(
+        NidoDaemonService(self.scheduler),
+        port=int(os.environ["NIDOD_RPC_PORT"]),
+        protocol_config={"allow_public_attrs": True, "allow_pickle": True},
+    )
 
-        See Stevens' "Advanced Programming in the UNIX
-        Environment" for details (ISBN 0201563177).
-        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-        """
-        try:
-            pid = os.fork()
-            if pid > 0:
-                # exit first parent
-                sys.exit(0)
-        except OSError as e:
-            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
-            sys.exit(1)
+    atexit.register(quit)
 
-        # decouple from parent environment
-        os.chdir(self.workdir)
-        os.setsid()
-        os.umask(0)
+    self.MQTTclient.loop_start()
+    self.scheduler.start()
+    self.RPCserver.start()  # Blocking
 
-        # do second fork
-        try:
-            pid = os.fork()
-            if pid > 0:
-                # exit from second parent
-                sys.exit(0)
-        except OSError as e:
-            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
-            sys.exit(1)
 
-        # redirect standard file descriptors
-        sys.stdout.flush()
-        sys.stderr.flush()
-        si = open(self.stdin, "r")
-        so = open(self.stdout, "a+")
-        se = open(self.stderr, "a+")
-        os.dup2(si.fileno(), sys.stdin.fileno())
-        os.dup2(so.fileno(), sys.stdout.fileno())
-        os.dup2(se.fileno(), sys.stderr.fileno())
+def quit():
+    self.controller.shutdown()
+    self.RPCserver.close()
+    self.scheduler.shutdown()
+    self.MQTTclient.disconnect()
+    self._l.info("Nido controller shutdown")
+    self._l.info("************************")
+    return
 
-        # Register function to call on exit
-        atexit.register(self.onstop)
-        # Register anonymous function for graceful exit on receiving
-        # SIGTERM
-        signal(SIGTERM, lambda signum, stack_frame: exit())
 
-        # write pidfile
-        pid = str(os.getpid())
-        with open(self.pidfile, "w+") as f:
-            f.write("%s\n" % pid)
+if __name__ == "__main__":
+    log_file = os.environ["NIDOD_LOG_FILE"]
 
-    def onstop(self):
-        self.quit()
-        os.remove(self.pidfile)
+    handler = logging.handlers.TimedRotatingFileHandler(
+        log_file, when="midnight", backupCount=7
+    )
+    formatter = logging.Formatter(
+        fmt="%(asctime)s [%(levelname)s] %(name)s | %(message)s",
+        datefmt="%d/%m/%Y %H:%M:%S",
+    )
+    handler.setFormatter(formatter)
+    root = logging.getLogger()
+    if "NIDO_DEBUG" in os.environ:
+        root.setLevel(logging.DEBUG)
+    else:
+        root.setLevel(logging.INFO)
+    root.addHandler(handler)
 
-    def start(self):
-        """
-        Start the daemon
-        """
-        # Check for a pidfile to see if the daemon already runs
-        try:
-            with open(self.pidfile, "r") as pf:
-                pid = int(pf.read().strip())
-        except IOError:
-            pid = None
-
-        if pid:
-            message = "pidfile %s already exists. Daemon already running?\n"
-            sys.stderr.write(message % self.pidfile)
-            sys.exit(1)
-
-        # Start the daemon
-        self._l.info("Starting Nido daemon")
-        self.daemonize()
-        self.run()
-
-    def stop(self):
-        """
-        Stop the daemon
-        """
-        # Get the pid from the pidfile
-        try:
-            with open(self.pidfile, "r") as pf:
-                pid = int(pf.read().strip())
-        except IOError:
-            pid = None
-
-        if not pid:
-            message = "pidfile %s does not exist. Daemon not running?\n"
-            sys.stderr.write(message % self.pidfile)
-            return None  # not an error in a restart
-
-        # Try killing the daemon process
-        try:
-            while 1:
-                os.kill(pid, SIGTERM)
-                time.sleep(0.1)
-        except OSError as err:
-            err = str(err)
-            if err.find("No such process") > 0:
-                if os.path.exists(self.pidfile):
-                    os.remove(self.pidfile)
-            else:
-                print(str(err))
-                sys.exit(1)
-
-    def restart(self):
-        """
-        Restart the daemon
-        """
-        self.stop()
-        self.start()
-
-    def run(self):
-        """
-        You should override this method when you subclass Daemon.
-
-        It will be called after the process has been daemonized by
-        start() or restart().
-        """
-
-    def quit(self):
-        """
-        You should override this method when you subclass Daemon.
-
-        It will be called before the process is stopped.
-        """
+    run()
